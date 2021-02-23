6974d54c637a6515be8a64fbad583b773988875b
LadyNick <nnikki1013@gmail.com>
2021-02-23 13:13:22 -0800

    adding more sms

--- source/main.c
+++ source/main.c
@@ -17,6 +17,9 @@
 
 unsigned char A0; //decrement
 unsigned char A1; //increment
+unsigned char go = 1;
+unsigned char change = 0;
+unsigned char system = 0; //initially off
 unsigned char num = 0x00;
 
 void transmit_data(unsigned char data) {
@@ -36,74 +39,112 @@ void transmit_data(unsigned char data) {
     PORTC = 0x00;
 }
 
-enum Register_States{display, inc, dec, reset, release}Register_State;
-int RegisterTick(int Register_State){
-	switch(Register_State){
-		case display:
-			transmit_data(num);
+enum Button_States{wait, inc, dec, both, release}Button_State;
+int ButtonTick(int Button_State){
+	switch(Button_State){
+		case wait:
 			if(A0){
-				Register_State = dec;
+				Button_State = dec;
 			}
 			else if(A1){
-				Register_State = inc;
+				Button_State = inc;
 			}
 			else if(A0 && A1){
-				Register_State = reset;
+				Button_State = both;
 			}
-			else{ Register_State = display;}
+			else{ Button_State = wait;}
 			break;
 		case inc:
-			if(num == 0xFF){
-				//do nothing, it stays the same because it's the highest
+			if(go == 3){
+				go = 1;
 			}
-			else{ ++num; }
-			transmit_data(num);
-			Register_State = release;
+			else{ ++go; }
+			Button_State = release;
 			break;
 		case dec:
-			if(num == 0x00){
-				//do nothing, stays the same because it's the lowest
+			if(go == 1){
+				go = 3;
 			}
-			else{ --num; }
-			transmit_data(num);
-			Register_State = release;
+			else{ --go; }
+			Button_State = release;
 			break;
-		case reset:
-			num = 0x00;
-			transmit_data(num);
-			Register_State = release;
+		case both:
+			if(change){
+				Button_State = release;
+			}
+			else{
+			if(system == 0 ){
+				system = 1;
+				go = 1;
+			}
+			else if(system == 1){
+				go = 1;
+				system = 0;
+			}
+			change = 1;
+			Button_State = release;
+			}
 			break;
 		case release:
 			if(!A0 && !A1){
-				Register_State = display;
+				Button_State = wait;
+				change = 0;
 			}
 			else if(A0 && A1){
-				Register_State = reset;
+				Button_State = both;
+			}
+			else{
+				Button_State = release;
 			}
 			break;
-		default: Register_State = display; break;
+		default: Button_State = wait; break;
 	}
-	return Register_State;
+	return Button_State;
 }
+
+enum 
 
 
 int main(void) {
     DDRC = 0xFF; PORTC = 0x00;
     DDRA = 0x00; PINA = 0xFF;
 
-    static task task1;
-    task *tasks[] = {&task1};
+    static task task1, task2, task3, task4, task5;
+    task *tasks[] = {&task1, &task2, &task3, &task4, &task5};
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
     const char start = -1;
 
-    //TASK1: Register + 2 Buttons
+    //Buttons
     task1.state = start;
     task1.period = 50; //manual didnt give specifics, but im choosing low anyway
     task1.elapsedTime = task1.period;
-    task1.TickFct = &RegisterTick;
+    task1.TickFct = &ButtonTick;
 
-    TimerSet(50); //just to match the 1 task there is
+    //Blinking all off and on
+    task2.state = start;
+    task2.period = 800; 
+    task2.elapsedTime = task2.period;
+    task2.TickFct = &Light1Tick;
+
+    //Blinking every other and switching
+    task3.state = start;
+    task3.period = 500;
+    task3.elapsedTime = task3.period;
+    task3.TickFct = &Light2Tick;
+
+    //Going inwards and outwards
+    task4.state = start;
+    task4.period = 100;
+    task4.elapsedTime = task4.period;
+    task4.TickFct = &Light3Tick;
+
+    task5.state = start;
+    task5.period = 1;
+    task5.elapsedTime = task5.period;
+    task5.TickFct = &DisplayTick;
+
+    TimerSet(1); //just to match the 1 task there is
     TimerOn();
     unsigned short i;
 
@@ -116,7 +157,7 @@ int main(void) {
 				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
 				tasks[i]->elapsedTime = 0;
 			}
-			tasks[i]->elapsedTime += 50;
+			tasks[i]->elapsedTime += 1;
 		}
 		while(!TimerFlag);
 		TimerFlag = 0;
